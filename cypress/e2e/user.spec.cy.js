de30b8f5d12d22a17430a33a0dc577101f83292c
RebecaCamilo <rebecacamilo3@gmail.com>
2024-05-09 23:43:34 -0300

    add combo box selectors

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -14,7 +14,9 @@ describe('Orange HRM Tests', () => {
     genericField: ".oxd-input--active",
     genericDateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
-    saveButton: "[type='submit']"
+    saveButton: "[type='submit']",
+    genericComboBox: ".oxd-select-text",
+    thirdItemComboBox: ":nth-child(3) > span"
   }
 
   it.only('User Info Update - Success', () => {
@@ -25,18 +27,27 @@ describe('Orange HRM Tests', () => {
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index")
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type("FirstName")
-    cy.get(selectorsList.lastNameField).clear().type("LastName")
-    cy.get(selectorsList.genericField).eq(3).clear().type("Employee")
-    cy.get(selectorsList.genericField).eq(4).clear().type("Id TEST")
-    cy.get(selectorsList.genericField).eq(5).clear().type("Driver's L Num")
-    cy.get(selectorsList.genericField).eq(8).clear().type("Test_Field TEST")
-    cy.get(selectorsList.genericDateField).eq(0).clear().type("2025-05-01") // data 1
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.saveButton).eq(0).click()
-    cy.get("body").should("contain", "Successfully Updated")
-    cy.get('.oxd-toast-close')
+    // cy.get(selectorsList.firstNameField).clear().type("FirstName")
+    // cy.get(selectorsList.lastNameField).clear().type("LastName")
+    // cy.get(selectorsList.genericField).eq(3).clear().type("Employee")
+    // cy.get(selectorsList.genericField).eq(4).clear().type("Id TEST")
+    // cy.get(selectorsList.genericField).eq(5).clear().type("Driver's L Num")
+    // cy.get(selectorsList.genericField).eq(8).clear().type("Test_Field TEST")
+    // cy.get(selectorsList.genericDateField).eq(0).clear().type("2025-05-01") // data 1
+    // cy.get(selectorsList.dateCloseButton).click()
+    // cy.get(selectorsList.saveButton).eq(0).click()
+    // cy.get("body").should("contain", "Successfully Updated")
+    // cy.get('.oxd-toast-close')
+    // cy.get(selectorsList.selectTextField).click();
+    // cy.get(selectorsList.selectTextField).eq(1)
+    // cy.get(selectorsList.selectTextField).eq(2)
+    cy.get(selectorsList.genericComboBox).eq(0).click()
+    cy.get(selectorsList.thirdItemComboBox).click()
+    cy.get(selectorsList.genericComboBox).eq(1).click()
+    cy.get(selectorsList.thirdItemComboBox).click()
   })
+
+
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
