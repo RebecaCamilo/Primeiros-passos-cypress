42ff5d438ab9d629cc704a58ecfae7c17a8240a2
RebecaCamilo <rebecacamilo3@gmail.com>
2024-05-12 20:44:10 -0300

    refactor login in page object arch

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,13 +1,14 @@
 import userData from "../fixtures/users/userData.json"
+import LoginPage from "../pages/loginPage.js"
+
+const loginPage = new LoginPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
+    
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
+
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
@@ -19,32 +20,33 @@ describe('Orange HRM Tests', () => {
     thirdItemComboBox: ":nth-child(3) > span"
   }
 
-  it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
+  it.only('User Info Update - Success', () => { 
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index")
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
-    // cy.get(selectorsList.firstNameField).clear().type("FirstName")
-    // cy.get(selectorsList.lastNameField).clear().type("LastName")
-    // cy.get(selectorsList.genericField).eq(3).clear().type("Employee")
-    // cy.get(selectorsList.genericField).eq(4).clear().type("Id TEST")
-    // cy.get(selectorsList.genericField).eq(5).clear().type("Driver's L Num")
-    // cy.get(selectorsList.genericField).eq(8).clear().type("Test_Field TEST")
-    // cy.get(selectorsList.genericDateField).eq(0).clear().type("2025-05-01") // data 1
-    // cy.get(selectorsList.dateCloseButton).click()
-    // cy.get(selectorsList.saveButton).eq(0).click()
-    // cy.get("body").should("contain", "Successfully Updated")
-    // cy.get('.oxd-toast-close')
-    // cy.get(selectorsList.selectTextField).click();
-    // cy.get(selectorsList.selectTextField).eq(1)
-    // cy.get(selectorsList.selectTextField).eq(2)
+    cy.get(selectorsList.firstNameField).clear().type("FirstName")
+    cy.get(selectorsList.lastNameField).clear().type("LastName")
+    cy.get(selectorsList.genericField).eq(3).clear().type("Employee")
+    cy.get(selectorsList.genericField).eq(4).clear().type("Id TEST")
+    cy.get(selectorsList.genericField).eq(5).clear().type("Driver's L Num")
+    cy.get(selectorsList.genericField).eq(8).clear().type("Test_Field TEST")
+    cy.get(selectorsList.genericDateField).eq(0).clear().type("2025-05-01") // data 1
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.saveButton).eq(0).click()
+    cy.get("body").should("contain", "Successfully Updated")
+    cy.get('.oxd-toast-close')
+    cy.get(selectorsList.selectTextField).click();
+    cy.get(selectorsList.selectTextField).eq(1)
+    cy.get(selectorsList.selectTextField).eq(2)
     cy.get(selectorsList.genericComboBox).eq(0).click()
     cy.get(selectorsList.thirdItemComboBox).click()
     cy.get(selectorsList.genericComboBox).eq(1).click()
     cy.get(selectorsList.thirdItemComboBox).click()
+    cy.get(selectorsList.genericComboBox).eq(2).click()
+    cy.get(':nth-child(5) > span').click()
   })
 
 
